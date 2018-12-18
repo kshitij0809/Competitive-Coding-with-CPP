fd96003f5876de5230fcd1bab10905146c4435ba
Kshitiz Pratap <kshitijpratap0809@gmail.com>
2018-12-18 17:41:09 +0530

    segregate zeros and ones

---
+++ Arrays/segregate_zero_one.cpp
@@ -0,0 +1,53 @@
+/*
+Segregate 0s and 1s in an array
+You are given an array of 0s and 1s in random order. Segregate 0s on left side and 1s on right side of the array. Traverse array only once.
+Input array   =  [0, 1, 0, 1, 0, 0, 1, 1, 1, 0] 
+Output array =  [0, 0, 0, 0, 0, 1, 1, 1, 1, 1] 
+*/
+
+// C program to sort a binary array in one pass 
+#include<stdio.h> 
+
+/*Function to put all 0s on left and all 1s on right*/
+void segregate0and1(int arr[], int size) 
+{ 
+	/* Initialize left and right indexes */
+	int left = 0, right = size-1; 
+
+	while (left < right) 
+	{ 
+		/* Increment left index while we see 0 at left */
+		while (arr[left] == 0 && left < right) 
+			left++; 
+
+		/* Decrement right index while we see 1 at right */
+		while (arr[right] == 1 && left < right) 
+			right--; 
+
+		/* If left is smaller than right then there is a 1 at left 
+		and a 0 at right. Exchange arr[left] and arr[right]*/
+		if (left < right) 
+		{ 
+			arr[left] = 0; 
+			arr[right] = 1; 
+			left++; 
+			right--; 
+		} 
+	} 
+} 
+
+/* driver program to test */
+int main() 
+{ 
+	int arr[] = {0, 1, 0, 1, 1, 1}; 
+	int i, arr_size = sizeof(arr)/sizeof(arr[0]); 
+
+	segregate0and1(arr, arr_size); 
+
+	printf("Array after segregation "); 
+	for (i = 0; i < 6; i++) 
+		printf("%d ", arr[i]); 
+
+	getchar(); 
+	return 0; 
+} 
